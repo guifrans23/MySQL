ef08043b710f7009499fd2101f202b84054f243d
Guifrans <87093840+legends23@users.noreply.github.com>
2022-05-16 16:44:31 -0300

    Add files via upload

---
+++ agenda.sql
@@ -0,0 +1,17 @@
+/**
+agenda de contatos
+@author guifrans
+*/
+
+-- verificar banco de dados
+show databases;
+
+-- Criar um novo banco de dados 
+create database teste;
+create database guifrans;
+
+-- excluir um banco de dados 
+drop database teste;
+
+-- Seleciona o banco de dados 
+use guifrans;
